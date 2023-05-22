87e4f73197d44e7762178d0b6451d241d9200085
suelen <sreginaclsilva@gmail.com>
2023-05-22 13:51:57 -0300

    final commit: finished project

--- src/repositories/ranking.repositories.js
+++ src/repositories/ranking.repositories.js
@@ -3,7 +3,7 @@ import { db } from "../database/database.connection.js"
 export function getRankingDB() {
     return db.query(`
 SELECT users.id AS id, users.name AS NAME,
-    COUNT(links.id) AS "linkCounts",
+    COUNT(links.id) AS "linksCount",
     COALESCE(SUM(links.views), 0) AS "visitCount"
 FROM users
     LEFT JOIN links on links."userId" = users.id
