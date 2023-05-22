6c831cdffe70f01feea64a70189cf4f60a61495c
suelen <sreginaclsilva@gmail.com>
2023-05-22 12:58:35 -0300

    feat: add controllers to GET ranking

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -1,6 +1,9 @@
+import { getRankingDB } from "../repositories/ranking.repositories.js"
+
 export async function getRanking(req, res){
     try {
-        
+        const ranking = await getRankingDB()
+        res.send(ranking.rows)
     } catch (error) {
         res.status(500).send(error.message)
     }
---
+++ src/repositories/ranking.repositories.js
@@ -0,0 +1,14 @@
+import { db } from "../database/database.connection.js"
+
+export function getRankingDB() {
+    return db.query(`
+SELECT users.id AS id, users.name AS NAME,
+    COUNT(links.id) AS "linkCounts",
+    COALESCE(SUM(links.views), 0) AS "visitCount"
+FROM users
+    LEFT JOIN links on links."userId" = users.id
+    GROUP BY users.id, users.name
+    ORDER BY "visitCount" DESC
+    LIMIT 10;
+    `)
+}
